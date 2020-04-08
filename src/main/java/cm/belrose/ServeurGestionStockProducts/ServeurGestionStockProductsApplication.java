419c8914675e75e5f9543a24cbc69ac73043c162
ngnawen <ngnawens@gmail.com>
2020-04-08 13:04:52 +0100

    First commit

---
+++ src/main/java/cm/belrose/ServeurGestionStockProducts/ServeurGestionStockProductsApplication.java
@@ -0,0 +1,13 @@
+package cm.belrose.ServeurGestionStockProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProductsApplication.class, args);
+	}
+
+}
